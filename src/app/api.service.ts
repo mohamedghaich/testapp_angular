1ece98470d2a5e8ff0371125ecf9bb8cb91f079d
mohamedghaich <simox15ghaich@gmail.com>
2023-06-14 15:49:13 +0100

    autocomplete

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -10,10 +10,10 @@ export class ApiService {
   constructor(private api:HttpClient) { }
 
   getpage(num:number):any{
-    return this.api.get("https://localhost:44304/api/clients?page="+num+"&pageSize=10");
+    return this.api.get("https://localhost:44304/api/clients?page="+num+"&pageSize=5");
   }
   getshearch(Cliente:string,numM:string,numf:string,prenom:string,nom:string,num:number):any{
-   return this.api.get("https://localhost:44304/api/clients/search?cliente="+Cliente+"&numeroDeMembro="+numM+"&numeroDeFamilia="+numf+"&prenomPer="+prenom+"&nomPer="+nom+"&page="+num+"&pageSize=10");
+   return this.api.get("https://localhost:44304/api/clients/search?cliente="+Cliente+"&numeroDeMembro="+numM+"&numeroDeFamilia="+numf+"&prenomPer="+prenom+"&nomPer="+nom+"&page="+num+"&pageSize=5");
   }
 
 }
