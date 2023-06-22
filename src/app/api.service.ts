462e823213e9978dd0793a56d9a7fd10fe5edd6e
mohamedghaich <simox15ghaich@gmail.com>
2023-06-22 11:06:11 +0100

    apihost

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -7,44 +7,44 @@ import { Observable, catchError, map, of } from 'rxjs';
   providedIn: 'root'
 })
 export class ApiService {
-
+  urlg:string="http://apicms.somee.com/";
   constructor(private api:HttpClient, private router: Router) { }
 
   getpage(num:number):any{
-    return this.api.get("https://localhost:44304/api/clients?page="+num+"&pageSize=8");
+    return this.api.get(this.urlg+"/api/clients?page="+num+"&pageSize=8");
   }
   getshearch(Cliente:string,numM:string,numf:string,prenom:string,nom:string,num:number):any{
-   return this.api.get("https://localhost:44304/api/clients/search?cliente="+Cliente+"&numeroDeMembro="+numM+"&numeroDeFamilia="+numf+"&prenomPer="+prenom+"&nomPer="+nom+"&page="+num+"&pageSize=8");
+   return this.api.get(this.urlg+"/api/clients/search?cliente="+Cliente+"&numeroDeMembro="+numM+"&numeroDeFamilia="+numf+"&prenomPer="+prenom+"&nomPer="+nom+"&page="+num+"&pageSize=8");
   }
   login(u:any): any {
-    const url = "https://localhost:44304/api/User/Login";
+    const url = this.urlg+"/api/User/Login";
     return this.api.post(url,u);
 
   }
   testlogin():any{
     const username = sessionStorage.getItem("username");
     const token = sessionStorage.getItem("token");
-    const url = "https://localhost:44304/api/User/CheckTokenValidity/"+username+"/"+token;
+    const url = this.urlg+"/api/User/CheckTokenValidity/"+username+"/"+token;
 
     return this.api.get(url);
   }
   logout(){
-    return this.api.delete("https://localhost:44304/api/User/Logout/"+sessionStorage.getItem("username"));
+    return this.api.delete(this.urlg+"/api/User/Logout/"+sessionStorage.getItem("username"));
   }
   getalluser():any{
-    return this.api.get("https://localhost:44304/api/User");
+    return this.api.get(this.urlg+"/api/User");
   }
   updateuser(user:string,u:any){
-    return this.api.put("https://localhost:44304/api/User/"+user,u);
+    return this.api.put(this.urlg+"/api/User/"+user,u);
   }
   adduser(u:any){
-    return this.api.post("https://localhost:44304/api/User",u);
+    return this.api.post(this.urlg+"/api/User",u);
   }
   deleteuser(user:string){
-    return this.api.delete("https://localhost:44304/api/User/"+user);
+    return this.api.delete(this.urlg+"/api/User/"+user);
   }
   getoneuser(username:string){
-    return this.api.get("https://localhost:44304/api/User/"+username);
+    return this.api.get(this.urlg+"/api/User/"+username);
   }
 
 
